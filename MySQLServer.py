8973300e14a62b413f44bcf992f8d9f8f05e18f1
root <jacklinemacharia99@gmail.com>
2025-01-30 03:30:41 -0800

    Let's Build Your Database: Your Gateway to Data Adventure!

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import errorcode
+try:
+    connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='hello'
+    )
+    cursor = connection.corsor()
+    try:
+        cursor.execute("CREATE DATABASE alx_book_store")
+        print("Database 'alx_book_store' created successfullt!")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print("Database 'alx_book_store' already exists.")
+        else:
+            print(f"Failed creating database: {err}")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("MySQL connection is closed.")
